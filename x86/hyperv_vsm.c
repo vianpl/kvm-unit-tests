78871533496d37364b367f61597fc1dea653b069
Evgeny Iakovlev <eyakovl@amazon.de>
2021-07-15 21:14:14 +0200

    hyper-v: implement HvGet/SetVpRegisters hypercalls
    Those hypercalls provide access to per-VTL registers for local and
    remote vcpus. We are going to need them to initialize VSM.
    
    Test newly added Get/Set VP registers hypercalls' handling of invalid
    input. Valid input will be tested by actually using them further to
    configure VSM.
    
    Signed-off-by: Evgeny Iakovlev <eyakovl@amazon.de>
    Reviewed-by: Anel Orazgaliyeva <anelkz@amazon.de>
    Reviewed-by: Sebastian Ott <sebott@amazon.de>
    
    CR: https://code.amazon.com/reviews/CR-69403579

--- x86/hyperv_vsm.c
+++ x86/hyperv_vsm.c
@@ -76,6 +76,162 @@ static inline void do_hypercall(struct hyperv_hypercall_thunk *hc)
 	hyperv_hypercall(vtl_partition_ctx()->hypercall_page, hc);
 }
 
+/* HvSetVpRegister hypercall */
+static uint64_t hv_set_vp_registers(uint32_t vp_index,
+                                    union hv_input_vtl target_vtl,
+                                    uint32_t* names,
+                                    struct hv_vp_register_val* vals,
+                                    size_t nregs,
+                                    bool fastcall)
+{
+	struct hv_vcpu *vcpu = vp_self();
+
+	/* 12 bit field for rep_cnt */
+	if (nregs > 0xFFF) {
+		report_abort("Cannot fit %zu registers into rep_cnt field", nregs);
+	}
+
+	struct hv_get_set_vp_registers args;
+	args.partition_id = HV_PARTITION_ID_SELF;
+	args.vp_index = vp_index;
+	args.input_vtl = target_vtl;
+
+	struct hyperv_hypercall_thunk hc = {0};
+	hc.fast = fastcall;
+	hc.code = HVCALL_SET_VP_REGISTERS;
+	hc.rep_cnt = nregs;
+	hc.rep_idx = 0;
+
+	if (fastcall) {
+		if (nregs > 4) {
+			report_abort("Cannot fit %zu registers in XMM call:", nregs);
+		}
+
+		hc.arg1 = ((uint64_t*)&args)[0];
+		hc.arg2 = ((uint64_t*)&args)[1];
+
+		/* 4 register names per xmm reg */
+		uint32_t* dst32 = (uint32_t*)(&hc.xmm[0]);
+		for (size_t i = 0; i < nregs; ++i) {
+			dst32[i] = names[i];
+		}
+
+		/* 1 register value per xmm reg, starting from the next register after whatever we used for names */
+		sse_reg* reg = &hc.xmm[ALIGN_UP(nregs, 4) / 4];
+		for (size_t i = 0; i < nregs; ++i) {
+			reg->q[0] = vals[i].low;
+			reg->q[1] = vals[i].high;
+			++reg;
+		}
+	} else {
+		hc.arg1 = virt_to_phys(vcpu->input_page);
+
+		void *pinput = vcpu->input_page;
+		pinput = push_input_data(pinput, &args, sizeof(args));
+		pinput = push_input_data(pinput, names, sizeof(*names) * nregs);
+		pinput = push_input_data(align_ptr_up(pinput, sizeof(vals)), vals, sizeof(*vals) * nregs);
+	}
+
+	while (hc.rep_idx < hc.rep_cnt) {
+		do_hypercall(&hc);
+		if (hc.result != HV_STATUS_SUCCESS) {
+			return hc.result;
+		}
+	}
+
+	return HV_STATUS_SUCCESS;
+}
+
+static inline void set_vp_register(uint32_t name, struct hv_vp_register_val val)
+{
+	uint64_t res = hv_set_vp_registers(HV_VP_INDEX_SELF, HV_NO_INPUT_VTL, &name, &val, 1, true);
+	assert_msg(HV_STATUS_SUCCESS == res, "Failed to write VP register 0x%x: %lx\n", name, res);
+}
+
+static inline void set_vp_register64(uint32_t name, uint64_t val)
+{
+	set_vp_register(name, (struct hv_vp_register_val){ .low = val, .high = 0 });
+}
+
+/* HvGetVpRegister hypercall */
+static uint64_t hv_get_vp_registers(uint32_t vp_index,
+                                    union hv_input_vtl target_vtl,
+                                    uint32_t* names,
+                                    struct hv_vp_register_val* vals,
+                                    size_t nregs,
+                                    bool fastcall)
+{
+	struct hv_vcpu *vcpu = vp_self();
+
+	/* 12 bit field for rep_cnt */
+	if (nregs > 0xFFF) {
+		report_abort("Cannot fit %zu registers into rep_cnt field", nregs);
+	}
+
+	struct hv_get_set_vp_registers args;
+	args.partition_id = HV_PARTITION_ID_SELF;
+	args.vp_index = vp_index;
+	args.input_vtl = target_vtl;
+
+	struct hyperv_hypercall_thunk hc = {0};
+	hc.fast = fastcall;
+	hc.code = HVCALL_GET_VP_REGISTERS;
+	hc.rep_cnt = nregs;
+	hc.rep_idx = 0;
+
+	if (fastcall) {
+		if (nregs > 4) {
+			report_abort("Cannot fit %zu registers in XMM call:", nregs);
+		}
+
+		hc.arg1 = ((uint64_t*)&args)[0];
+		hc.arg2 = ((uint64_t*)&args)[1];
+
+		/* 4 register names per xmm reg */
+		uint32_t* dst32 = (uint32_t*)(&hc.xmm[0]);
+		for (size_t i = 0; i < nregs; ++i) {
+			dst32[i] = names[i];
+		}
+	} else {
+		hc.arg1 = virt_to_phys(vcpu->input_page);
+		hc.arg2 = virt_to_phys(vcpu->output_page);
+
+		void *pinput = vcpu->input_page;
+		pinput = push_input_data(pinput, &args, sizeof(args));
+		pinput = push_input_data(pinput, names, sizeof(*names) * nregs);
+	}
+
+	while (hc.rep_idx < hc.rep_cnt) {
+		do_hypercall(&hc);
+		if (hc.result != HV_STATUS_SUCCESS) {
+			return hc.result;
+		}
+	}
+
+	if (fastcall) {
+		/* 1 register value per xmm reg, starting from the next register after whatever we used for names */
+		sse_reg* reg = &hc.xmm[ALIGN_UP(nregs, 4) / 4];
+		memcpy(vals, reg, nregs * sizeof(*vals));
+	} else {
+		memcpy(vals, vcpu->output_page, nregs * sizeof(*vals));
+	}
+
+	return HV_STATUS_SUCCESS;
+}
+
+static inline struct hv_vp_register_val get_vp_register(uint32_t name)
+{
+	struct hv_vp_register_val val;
+	uint64_t res = hv_get_vp_registers(HV_VP_INDEX_SELF, HV_NO_INPUT_VTL, &name, &val, 1, true);
+	assert_msg(HV_STATUS_SUCCESS == res, "Failed to read VP register 0x%x: %lx\n", name, res);
+	return val;
+}
+
+static inline uint64_t get_vp_register64(uint32_t name)
+{
+	return get_vp_register(name).low;
+}
+
 static void init_vcpu(void)
 {
 	uint32_t apicid = smp_id();
@@ -112,6 +268,74 @@ static inline void init_vtl_partition_ctx(void)
 		report_abort("Could not setup hypercall page");
 }
 
+static void test_get_set_vp_registers_negative(bool fast)
+{
+	uint32_t names[3] = { HV_X64_REGISTER_CR0, HV_X64_REGISTER_CR3, HV_X64_REGISTER_CR4 };
+	struct hv_vp_register_val vals[3] = {0};
+
+	/* Bad VP index */
+	report(
+		hv_get_vp_registers(0xacacacac, HV_INPUT_VTL(0), names, vals, 3, fast) == HV_STATUS_INVALID_VP_INDEX &&
+		hv_set_vp_registers(0xacacacac, HV_INPUT_VTL(0), names, vals, 3, fast) == HV_STATUS_INVALID_VP_INDEX,
+		"GetSetRegisters: Bad VP Index"
+	);
+
+	/* Bad VTL index */
+	report(
+		hv_get_vp_registers(HV_VP_INDEX_SELF, HV_INPUT_VTL(HV_INVALID_VTL), names, vals, 3, fast) != HV_STATUS_SUCCESS &&
+		hv_set_vp_registers(HV_VP_INDEX_SELF, HV_INPUT_VTL(HV_INVALID_VTL), names, vals, 3, fast) != HV_STATUS_SUCCESS,
+		"GetSetRegisters: Bad VTL index"
+	);
+
+	/* We should not be allowed to access VTL1 registers while in VTL0 */
+	report(
+		hv_get_vp_registers(HV_VP_INDEX_SELF, HV_INPUT_VTL(1), names, vals, 3, fast) == HV_STATUS_ACCESS_DENIED &&
+		hv_set_vp_registers(HV_VP_INDEX_SELF, HV_INPUT_VTL(1), names, vals, 3, fast) == HV_STATUS_ACCESS_DENIED,
+		"GetSetRegisters: Unpriviledged VTL"
+	);
+
+	/* Bad register id */
+	names[0] = 0xacacacac;
+	report(
+		hv_get_vp_registers(HV_VP_INDEX_SELF, HV_INPUT_VTL(0), names, vals, 3, fast) == HV_STATUS_INVALID_PARAMETER &&
+		hv_set_vp_registers(HV_VP_INDEX_SELF, HV_INPUT_VTL(0), names, vals, 3, fast) == HV_STATUS_INVALID_PARAMETER,
+		"GetSetRegisters: Bad register id"
+	);
+
+	/* Zero registers */
+	report(
+		hv_get_vp_registers(HV_VP_INDEX_SELF, HV_INPUT_VTL(0), names, vals, 0, fast) == HV_STATUS_SUCCESS &&
+		hv_set_vp_registers(HV_VP_INDEX_SELF, HV_INPUT_VTL(0), names, vals, 0, fast) == HV_STATUS_SUCCESS,
+		"GetSetRegisters: Zero register count"
+	);
+
+	if (fast) {
+
+		/*
+		 * Try to push more registers than XMM calls can handle.
+		 * Each XMM input reg fits 4 register names at most and 1 register value to output.
+		 * So given 6 registers total, specifying 5 and more names will not fit.
+		 *
+		 * We will call hypercalls directly here since we need to mess with thunk structure
+		 */
+
+		struct hyperv_hypercall_thunk hc = {0};
+		hc.fast = true;
+		hc.arg1 = HV_PARTITION_ID_SELF;
+		hc.arg2 = HV_VP_INDEX_SELF;
+		hc.rep_cnt = 5;
+		hc.rep_idx = 0;
+
+		hc.code = HVCALL_GET_VP_REGISTERS;
+		do_hypercall(&hc);
+		report(hc.result != HV_STATUS_SUCCESS, "GetRegisters: XMM count overflow");
+
+		hc.code = HVCALL_SET_VP_REGISTERS;
+		do_hypercall(&hc);
+		report(hc.result != HV_STATUS_SUCCESS, "SetRegisters: XMM count overflow");
+	}
+}
+
 int main(int ac, char **av)
 {
 	setup_vm();
@@ -141,6 +365,9 @@ int main(int ac, char **av)
 		init_vtl_vcpu_ctx();
 	}), NULL);
 	init_vtl_partition_ctx();
+
+	test_get_set_vp_registers_negative(true);
+	test_get_set_vp_registers_negative(false);
 
 summary:
 	return report_summary();
